8f6a1ae08a4d5e8ad0cd58f763ef68ef7f701f57
Александр Рябов <zkelo@yandex.ru>
2024-10-12 00:40:10 +0300

    Добавлен плагин Purge CSS для Vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import purgeCss from 'vite-plugin-purgecss-updated-v5'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue(), vueDevTools(), purgeCss({ variables: true })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
